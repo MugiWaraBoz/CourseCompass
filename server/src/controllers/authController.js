7c2e37308aa5b7f20678288e27e967a32d2e56bc
Nasib Islam <nasibislam2003@gmail.com>
2026-07-09 20:20:40 +0600

    Token Verification added

--- server/src/controllers/authController.js
+++ server/src/controllers/authController.js
@@ -135,6 +135,35 @@ const getStudent = async(req,res)=>{
 }
 
 // Token verification middleware
+function verifyToken(req, res, next) {
+    const authHeaders = req.headers['authorization']
+    const token = authHeaders && authHeaders.split(' ')[1]
+    if(!token){
+        return res.status(401).json({
+            success: false,
+            "error": { 
+                "code": "NO_TOKEN",
+                "message": "No token provided"
+            }
+        });
+    } else {
+        jwt.verify(token, process.env.JWT_SECRET,
+            (err, student) => {
+                if(err){
+                    return res.status(403).json({
+                        success: false,
+                        "error": {
+                            "code": "INVALID_TOKEN",
+                            "message": "Invalid token"
+                        }
+                    });
+                }
+                req.student = student;
+                next();
+            }
+        )
+    }
+}
 
 module.exports = {
     postRegister,
