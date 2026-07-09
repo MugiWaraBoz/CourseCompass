2a985ffe6b4fe58e6cc269a4a76685c7ae083b06
Nasib Islam <nasibislam2003@gmail.com>
2026-07-09 18:47:22 +0600

    Route query update for course
    added query based filters for course route

--- server/src/controllers/courseController.js
+++ server/src/controllers/courseController.js
@@ -3,12 +3,73 @@ const ObjectId = require("mongodb").ObjectId;
 
 const getCourses = async(req,res)=>{
     let db = database.getDb();
-    let data = await db.collection("Course").find({}).toArray();
     
+    const {
+        search,
+        department,
+        sortBy,
+        order,
+        page,
+        limit
+    } = req.query;
+
+    const filter = {}
+
+    if(department){
+        filter.department = department;
+    }
+
+    if(search){
+        /*
+            The $or operator is used to perform a logical OR operation on an array of conditions.
+            In this case, it is used to search for courses where either the name or code matches the search term.
+        */
+        filter.$or = [
+            /*
+                regex is used to perform pattern matching in MongoDB queries.
+                In this case, it is used to search for courses where the name or code 
+                contains the search term, regardless of case (case insensitive).
+            */
+            {name: { $regex: search, $options: "i"}}, // i = case insensitive
+            {code: { $regex: search, $options: "i"}}, // i = case insensitive
+        ];
+    }
+
+    const sort ={};
+    sort[sortBy] = order === "desc" ? -1 : 1;
+
+    const pageNumber = Number(page)
+    const limitNumber = Number(limit)
+    const skip = (pageNumber - 1) * limitNumber;
+
+    console.log(req.query);
+
+
+    let data = await db
+        .collection("Course")
+        .find(filter)
+        .sort(sort)
+        .skip(skip)
+        .limit(limitNumber)
+        .toArray();
+    
+    
+    const total = await db.collection("Course").countDocuments(filter);
+    console.log("Total courses found: ", total);
+    console.log("data_block: ", data);
+
     if(data.length > 0){
         res.status(200).json({
             success: true, 
-            data: {courses: data}
+            data: {
+                courses: data,
+                pagination: {
+                    page: pageNumber,
+                    limit: limitNumber,
+                    total,
+                    totalPages: Math.ceil(total / limitNumber)
+                },
+            },
         });
     } else {
         console.log("No courses found");
