22ab16aee9d3b6f0bde32a89a7425eae85fece79
Nasib Islam <nasibislam2003@gmail.com>
2026-08-14 22:59:22 +0600

    chore : indentation

--- server/src/app.js
+++ server/src/app.js
@@ -1,24 +1,24 @@
-require("dotenv").config() // loads .env file
+require('dotenv').config(); // loads .env file
 
-const express = require("express")
-const cors = require("cors")
+const express = require('express');
+const cors = require('cors');
 
 // Routes
-const auth = require("./routes/authRoutes")
-const course = require("./routes/courseRoutes")
-const faculty = require("./routes/facultyRoutes")
-const student = require("./routes/studentRoutes")
-const review = require("./routes/reviewRoutes")
+const auth = require('./routes/authRoutes');
+const course = require('./routes/courseRoutes');
+const faculty = require('./routes/facultyRoutes');
+const student = require('./routes/studentRoutes');
+const review = require('./routes/reviewRoutes');
 
-const app = express()
+const app = express();
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
-app.use("/auth", auth)
-app.use("/courses", course)
-app.use("/faculty", faculty)
-app.use("/students", student)
-app.use("/reviews", review)
+app.use('/auth', auth);
+app.use('/courses', course);
+app.use('/faculty', faculty);
+app.use('/students', student);
+app.use('/reviews', review);
 
-module.exports = app
+module.exports = app;
--- server/src/routes/studentRoutes.js
+++ server/src/routes/studentRoutes.js
@@ -1,24 +1,26 @@
-const express = require("express");
+const express = require('express');
 
-const ObjectId = require("mongodb").ObjectId;
-const verifyToken = require("../middleware/authMiddleware");
-const { getStudent,getStudentReviews, patchStudent } = require("../controllers/studentCotnroller");
-
+const ObjectId = require('mongodb').ObjectId;
+const verifyToken = require('../middleware/authMiddleware');
+const {
+  getStudent,
+  getStudentReviews,
+  patchStudent,
+} = require('../controllers/studentCotnroller');
 
 let studentRouter = express.Router();
 
 // Get the student information of the logged-in user
-studentRouter.route("/me").get(verifyToken, getStudent);
+studentRouter.route('/me').get(verifyToken, getStudent);
 
 // Get a student by studentId
 // Currently this route is not usable!!!
 // studentRouter.route("/:studentId").get(getStudent);
 
-
 // Update the student information of the logged-in user
-studentRouter.route("/me").patch(verifyToken, patchStudent);
+studentRouter.route('/me').patch(verifyToken, patchStudent);
 
 // Get reviews for a specific student
-studentRouter.route("/me/reviews").get(verifyToken, getStudentReviews);
+studentRouter.route('/me/reviews').get(verifyToken, getStudentReviews);
 
 module.exports = studentRouter;
